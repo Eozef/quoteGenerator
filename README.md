1f4a624b35f18c5260dc5f6b796afbcea0d0a467
Eozef <eozefow@gmail.com>
2021-01-04 04:17:56 +1100

    Add new Quote functionality and add Unknow quote

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-The Random Quote Generator is used to generate random quotations.
+The Random Quote Generator is used to generate random quotations, then tweet the Quote on the Twitter website.
--- script.js
+++ script.js
@@ -1,17 +1,35 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterButton = document.getElementById("twitter");
+const newQuoteButton = document.getElementById("new-quote");
+
 // API refers to https://forismatic.com/en/api/
 // Add a proxy Url to get around with the CORS policy, for info refers to https://medium.com/@dtkatz/3-ways-to-fix-the-cors-error-and-how-access-control-allow-origin-works-d97d55946d9
 
 async function getQuote() {
-  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const proxyUrl = "https://still-spire-54351.herokuapp.com/";
   const apiUrl =
     "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
   try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
-    console.log(data);
+    // add 'Unknown' for the quote with no author
+    if (data.quoteAuthor === "") {
+      authorText.innerText = "Unknown";
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+
+    // to-do reduce font size for long quotes
+    quoteText.innerText = data.quoteText;
   } catch (error) {
     console.log("Oops, no quote", error);
   }
+
+  // to-do Twitter Quote
+
+  newQuoteButton.addEventListener("click", getQuote);
 }
 
 // On Load
